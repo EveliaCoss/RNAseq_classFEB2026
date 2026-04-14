2db7afbd022c2b9263236c3cc403410410f69638
Evelia Coss <y0kocrozz@gmail.com>
2026-04-14 17:00:58 -0600

    actualizacion practica 2, clase y scripts

--- Practica_Dia2/scripts/STAR_index.sh
+++ Practica_Dia2/scripts/STAR_index.sh
@@ -1,6 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=star_index
+#SBATCH --output=/mnt/data/bioinfo-estadistica-2/RNAseq_2026/BioProject_2026/scripts/out_logs/star_index_%j.out
+#SBATCH --error=/mnt/data/bioinfo-estadistica-2/RNAseq_2026/BioProject_2026/scripts/out_logs/star_index_%j.err
+#SBATCH --ntasks=1                  # Un solo proceso
+#SBATCH --cpus-per-task=30          # Coincide con --runThreadN 30
+#SBATCH --mem=120G                  # Memoria suficiente para indexar GRCh38STA
+#SBATCH --mail-type=END             # Notificación al finalizar
+#SBATCH --mail-user=ecossnav@gmail.com       # Correo de notificación
+
+## --- Analisis de Fastqc ---
+# 1. Cargar el módulo de Trimmomatic
+module load kallisto/0.45.0
+cd /mnt/data/bioinfo-estadistica-2/RNAseq_2026/BioProject_2026
+
+# Paso 2. Generar index de STAR
 STAR --runThreadN 30 \
 --runMode genomeGenerate \
---genomeDir /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/STAR_index \
---genomeFastaFiles /mnt/Archives/genome/mouse/mm10/UCSC/chromosomes-1-file/mm10.fa \
---sjdbGTFfile /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/annotation/mm10.knownGene.gtf \
---sjdbOverhang 99
+--genomeDir ./align/STAR/STAR_index \
+--genomeFastaFiles ./reference/GCF_000001405.40_GRCh38.p14_genomic.fna.gz \
+--sjdbGTFfile ./reference/GCF_000001405.40_GRCh38.p14_genomic.gtf.gz \
+--sjdbOverhang 149
+
+# Parámetro --sjdbOverhang
+#   + Debe ser igual a la longitud de tus lecturas menos 1.
+#   + Ejemplo: si tus lecturas son de 100 pb → --sjdbOverhang 99.
+#   + Si son de 150 pb → --sjdbOverhang 149.
--- Practica_Dia2/scripts/kallisto_align.sh
+++ Practica_Dia2/scripts/kallisto_align.sh
@@ -3,12 +3,11 @@
 #SBATCH --job-name=kallisto
 #SBATCH --output=/mnt/data/bioinfo-estadistica-2/RNAseq_2026/BioProject_2026/scripts/out_logs/kallisto_%j.out
 #SBATCH --error=/mnt/data/bioinfo-estadistica-2/RNAseq_2026/BioProject_2026/scripts/out_logs/kallisto_%j.err
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
-#SBATCH --mail-type=END                      # Notificación al finalizar
+#SBATCH --ntasks=1                  # Un solo proceso
+#SBATCH --cpus-per-task=12          # 12 hilos para Kallisto (coincide con --threads 12)
+#SBATCH --mem=32G                   # Memoria suficiente para índices grandes (~30 GB)
+#SBATCH --mail-type=END             # Notificación al finalizar
 #SBATCH --mail-user=ecossnav@gmail.com       # Correo de notificación
-
 
 ## --- Analisis de Fastqc ---
 # 1. Cargar el módulo de Trimmomatic
@@ -16,14 +15,14 @@ module load kallisto/0.45.0
 cd /mnt/data/bioinfo-estadistica-2/RNAseq_2026/BioProject_2026
 
 # Paso 2. Generar index de kallisto
-kallisto index -i ./reference/transcripts.idx ./reference/Homo_sapiens.GRCh38.transcripts.fa.gz
+kallisto index -i ./align/kallisto/transcripts.idx ./reference/Homo_sapiens.GRCh38.transcripts.fa.gz
 
 # Paso 3. Pseudoalineamiento de secuencia Paired-end
 for file in ./data/processed/*_1.fq.gz                                                         # Read1
 do
   clean=$(echo $file | sed 's/^.\{15\}//g;s/_trimmed//')         # Nombre de la carpeta de salida, mismo nombre de SRA
   file_2=${clean}_2.fastq.gz                                           # Read2
-  kallisto quant --index ./reference/transcripts.idx \
+  kallisto quant --index ./align/kallisto/transcripts.idx \
                --output-dir ./kallisto_quant/${clean} \
                --threads 12 ${file} ${file_2}
 done
